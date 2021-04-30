8368c7116530482eeb2f3a95c5ac64981fdacaa5
Michelle AVOMO <michelle.avomo-ext@cellenza.com>
2021-04-30 15:38:12 +0200

    add main steps in kotlin

--- kotlin/src/main/kotlin/fr/codeworks/bbl/Interview.kt
+++ kotlin/src/main/kotlin/fr/codeworks/bbl/Interview.kt
@@ -1,5 +1,25 @@
 package fr.codeworks.bbl
 
 class Interview {
+    fun addCategorie(s: String) {
+        TODO("Not yet implemented")
+    }
+
+    fun loadQuestions() {
+        TODO("Not yet implemented")
+    }
+
+    fun askQuestions(): List<Response> {
+        TODO("Not yet implemented")
+    }
+
+    fun getScore(respons: List<Response>): Int{
+        TODO("Not yet implemented")
+    }
+
+    class Response {
+
+    }
+
 
 }
